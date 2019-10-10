732d29b653a9ebd86c171a2d0156d9a5a6a8fc29
Thomas Ebert <thomas.ebert@te-online.net>
2019-10-10 23:04:56 +0200

    Add unix timestamp

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const github = require("@actions/github");
 try {
   const time = new Date().toISOString();
   core.setOutput("time", time);
+  
+  const unixTime = Date.now();
+  core.setOutput("unixTime", unixTime);
 } catch (error) {
   core.setFailed(error.message);
 }
